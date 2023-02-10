7247886f51e74cbef237f7924f94a36ac1766092
leesavage09 <leesavage09@gmail.com>
2023-02-10 11:07:03 +0200

    update eslint and removed boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import '/components/styles/globals.css'
-import type { AppProps } from 'next/app'
+/* eslint-disable react/react-in-jsx-scope */
+import "/components/styles/globals.css"
+import type { AppProps } from "next/app"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />
 }
