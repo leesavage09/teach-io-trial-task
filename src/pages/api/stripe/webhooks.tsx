41be34da40a2e3f795897897df1f452fb3cfe8e3
leesavage09 <leesavage09@gmail.com>
2023-02-10 11:50:21 +0200

    updated webhook to decide what to persist

--- src/pages/api/stripe/webhooks.tsx
+++ src/pages/api/stripe/webhooks.tsx
@@ -1,17 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import Stripe from "stripe"
 
-const handleChargeSucceeded = (event: Stripe.Event) => {
-    // TODO save charge to mongodb database
-    console.log({ event })
+type Charge = {
+    chargeID: string;
+    customerID: string | Stripe.Customer | Stripe.DeletedCustomer | null;
+    amount: number;
+    currency: string;
+    created: number;
+    refunded: boolean;
+}
+
+const handleChargeSucceeded = (charge: Stripe.Charge) => {
+    const data: Charge = {
+        chargeID: charge.id,
+        customerID: charge.customer,
+        amount: charge.amount_captured,
+        currency: charge.currency,
+        created: charge.created,
+        refunded: charge.refunded
+    }
+
+    console.log(data)
+
+    // connected mongoose db
+    // create mongoose schema for the obj
+    // persist the ob
 
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-
+    // TODO no defense. Step 4: Secure your webhooks https://stripe.com/docs/webhooks#webhook-endpoint-four
     const event = req.body as Stripe.Event
 
-    if (event.type === "charge.succeeded") handleChargeSucceeded(event)
+    if (event.type === "charge.succeeded") handleChargeSucceeded(event.data.object as Stripe.Charge)
 
 
 
