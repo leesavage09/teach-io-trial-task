c000d4f7846685d5851731373cbc360d2fb28a0f
leesavage09 <leesavage09@gmail.com>
2023-02-10 12:41:28 +0200

    index page renders charges from the database

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
 /* eslint-disable react/react-in-jsx-scope */
 import Head from "next/head"
-import styles from "@components/styles/Home.module.css"
+import styles from "@components/styles/Home.module.scss"
+import { useEffect, useState } from "react"
+import { Charge } from "@components/db/models/charge"
+import axios from "axios"
 
 export default function Home() {
+    const [charges, setCharges] = useState<[Charge]>()
+
+    const update = () => {
+        axios.get("api/charges").then((result) => {
+            setCharges(result.data)
+        })
+    }
+
+    useEffect(() => update(), [])
+
     return (
         <>
             <Head>
@@ -12,9 +25,40 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className={styles.main}>
-                <div className={styles.description}>
-
-                </div>
+                <button>Refresh Charges</button>
+                <h1>Charges</h1>
+                <ul>
+                    {charges && charges.map((charge) => (
+                        <li key={charge.chargeID}>
+                            <h2>{charge.chargeID}</h2>
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td>customerID</td>
+                                        <td>{ charge.customerID}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>amount</td>
+                                        {/* TODO just rendering £. will need better rendering */}
+                                        <td>£{charge.amount / 100}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>currency</td>
+                                        <td>{charge.currency}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>created</td>
+                                        <td>{new Date(charge.created * 1000).toLocaleString()}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <p>{charge.refunded && "This Charge has been refunded"}</p>
+                            {!charge.refunded && (
+                                <button onClick={() => console.log("TODO")}>Refund</button>
+                            )}
+                        </li>
+                    ))}
+                </ul>
             </main>
         </>
     )
